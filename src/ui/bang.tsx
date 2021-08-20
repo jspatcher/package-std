3d22d371d3a9bba8bb93796cd81ff879d1c017e6
Fr0stbyteR <renshihong@hotmail.com>
2021-08-20 20:20:47 +0800

    fix bang doubleclick UI

--- src/ui/bang.tsx
+++ src/ui/bang.tsx
@@ -2,7 +2,9 @@ import { React, DefaultUI, Bang } from "../sdk";
 import type { bang } from "../index.jspatpkg";
 
 export default class BangUI extends DefaultUI<bang> {
-    handleDoubleClick = (e: React.MouseEvent<HTMLDivElement>) => this.object.outlet(0, new Bang());
+    handleDoubleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (this.editor.state.locked) this.object.outlet(0, new Bang());
+    }
     render() {
         return (
             <DefaultUI {...this.props} containerProps={{ ...this.props.containerProps, onDoubleClick: this.handleDoubleClick }} />
